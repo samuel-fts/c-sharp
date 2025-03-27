66b3a07881b686ddbdfea646eaa32a4967f0848c
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-03-27 11:13:40 -0300

    novas aulas de c#

---
+++ aulas/aula07/aula07.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Principal{
+    class Aula07{
+        static void Main(){
+            const string canal="CFB Cursos";// constantes nao se podem mudar seu valor uma vez atribuida
+            const double pi=3.1415;//double, float e decimal se diferenciam pela precisao dos decimais
+
+            Console.WriteLine("canal {0}\n Pi: {1}", canal, pi);
+        }
+    }
+}
