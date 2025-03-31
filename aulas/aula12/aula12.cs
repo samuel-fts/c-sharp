f1a38611f4111127d7d440a1bf8819c14a82c563
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-03-31 09:31:31 -0300

    novas aulas

--- aulas/aula12/aula12.cs
+++ aulas/aula12/aula12.cs
@@ -7,7 +7,7 @@ namespace Principal{
 
             num = int.Parse(Console.ReadLine());// um cast de conversao para a escrita numeral se tranformar em um int, entao usase Parse e nao (int)
 
-            if(num){
+            if(num == 1){
                 Console.WriteLine("existe num");
             }
         }
