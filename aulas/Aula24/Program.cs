f88cd3bfa198e63d52d046607636e10bfea89768
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-04-02 13:57:44 -0300

    novas aulas

---
+++ aulas/Aula24/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Principal
+{
+    class Aula24
+    {
+        static void Main()
+        {
+            int inteiro = 100;
+            float real = 25.5f;
+
+            // real = inteiro;
+            // inteiro = (int)real;
+            //parse so aceita texto, entao tem que colocar dentro de parenteses
+            inteiro = int.Parse(real.ToString());//inteiro vai receber o parse do alia int para real que e um floar para string
+
+            inteiro = Convert.ToInt32(real);// inteiro recebe o uso da class convert dentro do metodo ToInt32 passando o parametro real para ser convertido e arredondado
+
+            switch (inteiro)
+            {
+                case 100: Console.WriteLine("100"); break;
+                default: Console.WriteLine("errado"); break;
+            }
+
+        }
+    }
+}
---
+++ aulas/Aula25/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Principal
+{
+    class Aula25
+    {
+        static void Main()
+        {
+            string nomeCompleto = ConcactNome("samuel", "bruna");
+            Console.WriteLine(nomeCompleto);
+        }
+
+        static string ConcactNome(string nome, string sobrenome)
+        {
+            return nome + sobrenome;
+        }
+    }
+}
