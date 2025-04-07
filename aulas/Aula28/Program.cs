e895d42d9a833919e68c77dcdc8e7464fe860a20
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-04-07 19:57:24 -0300

    novas aulas

---
+++ aulas/Aula28/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Principal
+{
+    class Aula28
+    {
+        static void Main()
+        {
+            var id = Guid.NewGuid();
+
+            id.ToString();
+
+            id = new Guid("186237d5-a1aa-4cbb-8ed6-9eff7324308e");
+            Console.WriteLine(id);
+
+
+        }
+    }
+}
