091cda294965e42f38eaa8bbed578eaec677936c
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-04-09 13:08:06 -0300

    novas aulas

---
+++ aulas/Aula30/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Principal
+{
+    class Aula30
+    {
+        static void Main()
+        {
+            Console.Clear();
+            var data = new DateTime(2000, 12, 12, 14, 43, 12);
+            // var data = DateTime.Now;
+
+            var formatado = String.Format("{0:yyyy}", data);//para formatacao deve ser colocado dentro de chaves junto ao 0
+            Console.WriteLine(formatado);
+        }
+    }
+}
