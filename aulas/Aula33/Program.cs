021f8bb2f34ebfbf6db3682e5518aafc64299cfb
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-04-10 12:20:31 -0300

    novas uals

---
+++ aulas/Aula33/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Pricipal
+{
+    class Aula33
+    {
+        static void Main()
+        {
+            var arr = new int[3];
+            try
+            {
+                for (int index = 0; index < 10; index++)
+                {
+                    Console.WriteLine(arr);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ops.algo due errado");
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
