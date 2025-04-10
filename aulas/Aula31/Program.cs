71f9798e3d3bda48a2e28514cb0c86bcea10a983
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-04-10 10:46:04 -0300

    novas aulas

---
+++ aulas/Aula31/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+namespace Pricipal
+{
+    class Aula31
+    {
+        static void Main()
+        {
+            var pt = new CultureInfo("pt-BR");
+            var atual = CultureInfo.CurrentCulture;
+
+            Console.WriteLine(DateTime.Now.ToString("D", pt));
+        }
+    }
+}
