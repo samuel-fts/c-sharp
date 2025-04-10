f80c8b25f0ee8931ce12d82ea082cc1d36dd8302
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-04-10 10:58:32 -0300

    novas aulas

---
+++ aulas/Aula32/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Principal
+{
+    class Aula32
+    {
+        static void Main()
+        {
+            Console.Clear();
+            decimal valor = 10.25m;
+            valor = Math.Round(valor);//arredonda de forma automatica, dependendo do  valor depois da virgula
+            valor = Math.Ceiling(valor);//arredonda pra cima
+            valor = Math.Floor(valor);//arredonda pra baixo
+            Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));//pega de dentro da culture info o metodo de criar a formatacao do valor dado no parametro
+        }
+    }
+}
