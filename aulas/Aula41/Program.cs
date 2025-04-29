c98932d75b5c78ee2a1ac6fc8995a8bafdffb5f1
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-04-29 09:36:44 -0300

    novas aulas

---
+++ aulas/Aula41/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Principal
+{
+    class Aula40
+    {
+        static void Main()
+        {
+            Soma(2, 4, 2, 4);
+        }
+        static void Soma(params int[] n)//o params da a possibilidade de indicar mais de um parametro dentro da funcao 
+        {
+            int res = 0;
+            if (n.Length < 1)
+            {
+                Console.WriteLine("nao exitem parametros a serem somados");
+            }
+            else if (n.Length == 1)
+            {
+                Console.WriteLine("adicione mais 1 numero a ser somado");
+            }
+            else
+            {
+                for (int i = 0; i < n.Length; i++)
+                {
+                    res += n[i];
+
+                }
+                Console.WriteLine($"A soma dos valores deu {res}");
+            }
+        }
+    }
+}
---
+++ aulas/Aula42/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Principal
+{
+    class Aula42
+    {
+        static void Main()
+        {
+            Jogador j1 = new Jogador(29, true);
+            Console.WriteLine("Energia do jogador {0}", j1.Energia);
+        }
+
+
+    }
+    public class Jogador
+    {
+        public int Energia;
+        public bool Vivo;
+        public Jogador(int energia, bool vivo)
+        {
+            Energia = energia;
+            Vivo = vivo;
+        }
+    }
+}
