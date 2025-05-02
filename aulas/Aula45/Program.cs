3d62bf5c6b5bd164a8c3f54f1de045a62069a91c
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-05-02 08:55:28 -0300

    novas aulas

---
+++ aulas/Aula45/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pricipal
+{
+    class Aula45
+    {
+        static void Main()
+        {
+
+        }
+    }
+    class Calculos
+    {
+        public int V1;
+        public int V2;
+
+        public Calculos(int v1, int v2)
+        {
+            this.V1 = v1;
+            this.V2 = v2;
+        }
+
+        public int Somar()
+        {
+            return V1 + V2;
+        }
+    }
+}
