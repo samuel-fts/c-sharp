5cbfa36982b2880bbd9f61cdd50eda4a91920f59
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-05-05 10:54:42 -0300

    novas aulas

---
+++ aulas/Aula47/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Principal
+{
+    public class Base
+    {
+        public Base()
+        {
+            Console.WriteLine("Base");
+        }
+        virtual public void Info()
+        {
+
+        }
+    }
+
+    public class Derivada : Base
+    {
+        public Derivada()
+        {
+            Console.WriteLine("Derivada1");
+        }
+        override public void Info()
+        {
+            Console.WriteLine("foi sobrescrito");
+        }
+    }
+    class Aula47
+    {
+        static void Main()
+        {
+            Base rif;
+            Derivada derivada = new Derivada();
+            rif = derivada;
+            rif.Info();
+        }
+    }
+}
