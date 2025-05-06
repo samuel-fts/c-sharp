4d229ee96739e7aa6c19bec3bb7ad0cdeebf93b7
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-05-06 09:25:48 -0300

    novas aulas

---
+++ aulas/Aula49/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pricipal
+{
+    struct Carro
+    {
+        public string Marca;
+        public string Modelo;
+
+        public string Cor;
+
+        public Carro(string marca, string modelo, string cor)
+        {
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.Cor = cor;
+        }
+    }
+    class Aula49
+    {
+        static void Main()
+        {
+            Carro c1 = new Carro("vk", "golf", "prata");
+            Carro[] carros = new Carro[2];
+
+            carros[0].Modelo = "HRV";
+
+        }
+    }
+}
