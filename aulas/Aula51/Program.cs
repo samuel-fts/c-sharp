73c2a5faf8f644075cad4fe9a3f2fc0a2107f867
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-05-09 09:08:31 -0300

    recursividade

---
+++ aulas/Aula51/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Principal
+{
+    class Aula51
+    {
+        static void Main()
+        {
+            Calc calc = new Calc();
+            var res = calc.Fat(5);
+            Console.WriteLine(res);
+        }
+    }
+    class Calc
+    {
+        public int Fat(int n)
+        {
+            int res;
+            if (n <= 1)
+            {
+                res = 1;
+            }
+            else
+            {
+                res = n * Fat(n - 1);
+            }
+            return res;
+        }
+    }
+}
