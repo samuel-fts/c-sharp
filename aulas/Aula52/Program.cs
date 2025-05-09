b2c061004c33f052f253dc02fd450023e868e1b8
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-05-09 09:18:57 -0300

    metodos e variaveis statics

---
+++ aulas/Aula52/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Principal
+{
+    class Mat
+    {
+        public static int Dobrar(int n)
+        {
+            return n * 2;
+        }
+    }
+    class Aula52
+    {
+        static void Main()
+        {
+            Console.WriteLine(Mat.Dobrar(2));//posso chamar o metodo static de uma classe nao static sem precisar instanciar ela
+        }
+    }
+}
