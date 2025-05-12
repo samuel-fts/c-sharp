3df8ed018d4977aafb7743fb8fc59fbb83f15493
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-05-12 08:28:04 -0300

    dictionary

---
+++ aulas/Aula55/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Principal
+{
+    class Aula55
+    {
+        static void Main()
+        {
+            Dictionary<int, string> veiculos = new Dictionary<int, string>();
+
+            veiculos.Add(10, "moto");
+            veiculos.Add(20, "navio");
+
+            veiculos.Clear();//limpa o dicionario
+
+
+            Console.WriteLine("Tamanho do dictionary: {0}", veiculos.Count);//mostra a qunatidade de items dentro do dicionario
+            int chave = 22;
+            if (veiculos.ContainsKey(chave))
+            {
+                Console.WriteLine("esta na colecao");
+            }
+
+            veiculos[10] = "carro";
+
+
+        }
+    }
+}
