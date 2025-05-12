a0f1f91a006dc019d9fd895b7558561a1feb9ce6
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-05-12 08:15:37 -0300

    trychatch

---
+++ aulas/Aula54/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Principal
+{
+    class Aula54
+    {
+        static void Main()
+        {
+            int n1, n2, res;
+            n1 = n2 = res = 0;
+
+            n1 = 12;
+            n2 = 0;
+
+            try
+            {
+                res = n1 / n2;
+                Console.WriteLine("{0}/{1}={2}", n1, n2, res);
+                throw new Exception("erro curso");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro:{0}", e.Message);
+            }
+
+        }
+    }
+}
