c968b71241d754efa6e9c0d05041b27606db4dd8
Samuel Freitas <samuel.fts.s.8@gmail.com>
2025-06-07 08:05:08 -0300

    linkedlist

---
+++ aulas/Aula56/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pricipal
+{
+    class Aula56
+    {
+        static void Main()
+        {
+            LinkedList<string> transp = new LinkedList<string>();
+            transp.AddFirst("carro");
+            transp.AddFirst("moto");
+            transp.AddFirst("onibus");
+            transp.AddFirst("aviao");
+
+            LinkedListNode<string> no;
+            no = transp.FindLast("carro").Next;
+            transp.AddAfter(no, "patins");
+
+            transp.Remove("carro");
+            transp.Clear();
+            transp.RemoveFirst();
+            transp.RemoveLast();
+
+
+            foreach (string t in transp)
+            {
+                Console.WriteLine("transporte:{0}", t);
+            }
+        }
+    }
+}
